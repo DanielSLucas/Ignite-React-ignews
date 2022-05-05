d3c6c2586065baf495fa765da77b909d9a085359
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-05 14:14:17 -0300

    static site generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {  
+export const getStaticProps: GetStaticProps = async () => {  
   // pegar um produto (retrive) por id (API ID do produto)
   // como segundo parâmetro poderiamos passar um objeto com a proprieadade
   // expand: ["product"] que permitiria pegar mais informações do produto
@@ -53,6 +53,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours - cria uma página estática a cada 24h
   }
 }
