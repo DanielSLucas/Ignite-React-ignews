bdc58b588217ad9a3e481a82a81979d886cbd2ec
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-06 09:59:00 -0300

    autenticação com next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,17 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from "next-auth/react"
 
 import styles from './styles.module.scss';
 
 export function SignInButton () {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession();  
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361"/>
       Daniel Lucas
@@ -19,6 +21,7 @@ export function SignInButton () {
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with Github
