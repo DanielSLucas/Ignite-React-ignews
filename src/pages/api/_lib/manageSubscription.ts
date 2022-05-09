31533845cfb4bd7de2447e3f93b4de35156cdd15
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-09 17:33:52 -0300

    ouvindo mais eventos

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,6 +5,7 @@ import { stripe } from "../../../services/stripe";
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
+  createAction = false,
 ) {
   // Buscar usuário no FaunaDB com o customerId
   const userRef = await fauna.query(
@@ -29,11 +30,30 @@ export async function saveSubscription(
     price_id: subscription.items.data[0].price.id,
   } 
 
-  // Salvar os dados da subscription no FaunaDB
-  await fauna.query(
-    q.Create(
-      q.Collection('subscriptions'),
-      { data: subscriptionData }
+  
+
+  if (createAction) {
+    // Salvar os dados da subscription no FaunaDB
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData }
+      )
     )
-  )
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(
+              q.Index('subscription_by_id'),
+              subscriptionId
+            )
+          )
+        ),
+        { data: subscriptionData }
+      )
+    )
+  }
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -25,7 +25,9 @@ export const config = {
 };
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',  
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
 ]);
 
 export default async function webhooks(req: NextApiRequest, res: NextApiResponse) {
@@ -45,7 +47,18 @@ export default async function webhooks(req: NextApiRequest, res: NextApiResponse
 
     if(relevantEvents.has(type)) {
       try {
-        switch (type) {
+        switch (type) {          
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false,
+            );
+
+            break;
           case 'checkout.session.completed':
 
             const checkoutSession = event.data.object as Stripe.Checkout.Session;
@@ -53,6 +66,7 @@ export default async function webhooks(req: NextApiRequest, res: NextApiResponse
             await saveSubscription(
               checkoutSession.subscription.toString(),
               checkoutSession.customer.toString(),
+              true,
             );
 
             break;
