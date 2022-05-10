b6f7823b95b1bee7a1c90a981a744b1df5c8558e
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-10 12:01:28 -0300

    navegação no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 import SignInButton from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { asPath } = useRouter();
+  
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="ig.news" />
+          </a>
+        </Link>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={asPath === '/' ? styles.active : ''}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+            <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
