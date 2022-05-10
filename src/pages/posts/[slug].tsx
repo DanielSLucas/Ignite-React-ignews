f95434f209ada1c4458fa111060fcee9c8f823ee
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-10 14:25:06 -0300

    validando assinatura ativa

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,7 +41,16 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req });
   const { slug } = params;
 
-  // if (!session) {}
+  console.log(session);
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   const prismic = createClient({ req });
 
