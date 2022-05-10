93e7d8ca008150dd5cd5a1049fd8b1f2edff3122
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-05-10 11:51:41 -0300

    listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,22 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import Prismic from '@prismicio/client';
+import { asText } from '@prismicio/helpers';
 
 import { createClient } from '../../services/prismicio';
 
 import styles from '../../styles/posts.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -15,30 +25,15 @@ export default function Posts() {
       
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href='#'>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo wiht Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn hwo to create a Monorepo to manage multriple packaeges
-              with a shared build, test, and release process.
-            </p>
-          </a>
-          <a href='#'>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo wiht Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn hwo to create a Monorepo to manage multriple packaeges
-              with a shared build, test, and release process.
-            </p>
-          </a>
-          <a href='#'>
-            <time>12 de março de 2021</time>
-            <strong>Creating a Monorepo wiht Lerna & Yarn Workspaces</strong>
-            <p>
-              In this guide, you will learn hwo to create a Monorepo to manage multriple packaeges
-              with a shared build, test, and release process.
-            </p>
-          </a>
+          {posts.map(post => (
+            <a href='#' key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>
+                {post.excerpt}
+              </p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -52,10 +47,23 @@ export const getStaticProps: GetStaticProps = async ({previewData}) => {
     fetch: ['posts.title', 'posts.content'],
     pageSize: 100, 
   });
-  
-  console.log(response);
+
+  const posts = response.map(post => {
+    return {
+      slug: post.uid,
+      title: asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === "paragraph")?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',  {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+    }
+  })
 
   return {
-    props: {}
-  }
+    props: {
+      posts,
+    }
+  };
 }
