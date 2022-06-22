c50a8030075d3191a6500d749d1a89c03432fdff
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-06-22 16:02:09 -0300

    fix useless import

--- src/pages/slice-simulator.tsx
+++ src/pages/slice-simulator.tsx
@@ -1,14 +1,13 @@
 import { SliceSimulator } from "@prismicio/slice-simulator-react";
 import { SliceZone } from "@prismicio/react";
 
-import { components } from "../../slices";
 import state from "../../.slicemachine/libraries-state.json";
 
 const SliceSimulatorPage = () => {
   return (
     <SliceSimulator
       sliceZone={({ slices }) => (
-        <SliceZone slices={slices} components={components} />
+        <SliceZone slices={slices} />
       )}
       state={state}
     />
